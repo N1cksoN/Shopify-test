c95dbbde8f22a15615b5ebda85a6b02cf8f3a80a
Nikita Shevchenko <nicksonsheva0609@gmail.com>
2020-10-21 18:28:04 +0200

    added discounts + navbars

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "a756a904e91f7261343c";
+/******/ 	var hotCurrentHash = "17c76e20995343e67c54";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
 import { EmptyState, Layout, Page } from '@shopify/polaris';
+import { TitleBar } from '@shopify/app-bridge-react';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
 
 const Index = () => (
     <Page>
+         <TitleBar
+      title="Sample App"
+      primaryAction={{
+        content: 'Select products',
+      }}
+    />
         <Layout>
             <EmptyState
                 heading="Discount your products temporarily"
@@ -11,7 +18,7 @@ const Index = () => (
                 content: 'Select products',
                 onAction: () => console.log('clicked'),
                 }}
-                image={img}
+                image={ img }
             >
                 <p>Select products to change their price temporarily.</p>
         </EmptyState>
